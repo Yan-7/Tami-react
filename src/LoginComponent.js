6016e4947513bda34a00d2abe9af2242edd1c3e1
Yan <yanivalmagor@gmail.com>
2023-10-23 13:28:48 +0300

    23.10

--- src/LoginComponent.js
+++ src/LoginComponent.js
@@ -1,9 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 import { useState } from 'react'
 
+
 // Initialize Supabase client
-const supabaseUrl = 'https://qngmdybyrtcajqjohpnt.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFuZ21keWJ5cnRjYWpxam9ocG50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYzMjE2NzEsImV4cCI6MjAxMTg5NzY3MX0.Jd6gPs09nEaWIIqsmO4ysYYzaW7hgQtRSxJ4EYRsRoA'
+const supabaseUrl = 'https://sdbrfxiprfitnpcmwhmg.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkYnJmeGlwcmZpdG5wY213aG1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc1NzAyMDYsImV4cCI6MjAxMzE0NjIwNn0.3sxMQWZf16N9uGoPzeKzP21colvfWJJ0g50ZIlh4HQk';
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 const LoginComponent = () => {
